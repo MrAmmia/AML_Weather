5b72e2c1880accf7ac5be5b727290ddf9050237e
Musabdulhim3 <musabdulhim3@gmail.com>
2022-06-24 22:33:39 +0100

    Created viewpager and designed every layout

--- app/src/main/java/net/thebookofcode/www/amlweather/MainActivity.kt
+++ app/src/main/java/net/thebookofcode/www/amlweather/MainActivity.kt
@@ -3,27 +3,44 @@ package net.thebookofcode.www.amlweather
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import net.thebookofcode.www.amlweather.adapter.ViewPagerAdapter
+import net.thebookofcode.www.amlweather.databinding.ActivityMainBinding
 import net.thebookofcode.www.amlweather.model.MainViewModel
 import net.thebookofcode.www.amlweather.model.MainViewModelFactory
 import net.thebookofcode.www.amlweather.repository.Repository
+import net.thebookofcode.www.amlweather.ui.CurrentWeather
+import net.thebookofcode.www.amlweather.ui.FutureWeatherFragment
+import net.thebookofcode.www.amlweather.ui.OtherCitiesFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     private lateinit var viewModel: MainViewModel
-    private lateinit var txtView:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        txtView = findViewById(R.id.txtView)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         val repository = Repository()
         val mainViewModelFactory = MainViewModelFactory(repository)
         viewModel = ViewModelProvider(this,mainViewModelFactory)[MainViewModel::class.java]
-        /*viewModel.getWeatherByLocation(6.543941,3.3680577)
+        viewModel.getWeatherByLocation(6.543941,3.3680577)
         viewModel.myResponseByLocation.observe(this, Observer {
             Log.i("Response",it.body().toString())
-            //txtView.text = it.body().toString()
-        })*/
+            it.body().
+        })
+
+        val fragmentList = arrayListOf<Fragment>(
+            CurrentWeather(),
+            FutureWeatherFragment(),
+            OtherCitiesFragment()
+        )
+        val adapter = ViewPagerAdapter(
+            fragmentList,
+            this.supportFragmentManager,
+            lifecycle
+        )
+        binding.viewPager2.adapter = adapter
     }
 }
